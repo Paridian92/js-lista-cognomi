a371497cb6f80de655f4b5ce32796577b82f23a5
Paridian92 <paridian92@hotmail.it>
2021-04-12 17:04:20 +0200

    x ticket

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//creo il mio array di cognomi
+
+var a = [
+  'Bianchi',
+  'Neri',
+  'Rossi',
+  'Verdi',   
+  'Gialli'
+];
+
+var listaCognomi = a
+
+listaCognomi.push =prompt('inserisci il cognome')
+
+console.log(a)
+
+
+
+
+
+/* //pusho il mio cognome nell'array
+listaCognomi.join = prompt('inserisci il tuo cognome')
+
+for (var i =0; i<listaCognomi.length; i++){
+  var elemento_lista = listaCognomi[i];
+  
+  var elemento_precedente = document.getElementById('lista').innerHTML;
+  
+  document.getElementById('lista').innerHTML= elemento_precedente + "<li>" + elemento_lista + "</li>";
+} */
